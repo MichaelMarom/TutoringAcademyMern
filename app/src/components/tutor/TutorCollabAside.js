0b80ca777c419518e55439ccf47b2b04ff878838
asiya1330 <asiya.batool987@gmail.com>
2023-11-01 06:40:14 -0400

    change collab

--- app/src/components/tutor/TutorCollabAside.js
+++ app/src/components/tutor/TutorCollabAside.js
@@ -2,17 +2,17 @@ import { useEffect, useState } from 'react';
 import screenLarge from '../../images/screen-full-svgrepo-com.svg';
 import screenNormal from '../../images/screen-normal-svgrepo-com.svg'
 import muteSvg from '../../images/mute-svgrepo-com.svg'
-import DiableVideoImage from '../../images/video-recorder-off-svgrepo-com.svg'; 
+import DiableVideoImage from '../../images/video-recorder-off-svgrepo-com.svg';
 import { useLocation } from 'react-router-dom';
 import { socket } from '../../socket';
-import {Peer} from 'peerjs';
+import { Peer } from 'peerjs';
 
 const TutorAside = () => {
 
     let [mssg, setMssg] = useState('hi');
     let [mssgList, setMssgList] = useState([]);
 
-    let location = useLocation(); 
+    let location = useLocation();
     let [authorization, setAuthorization] = useState(false);
     let [peerId, setPeerId] = useState('')
     let [conn, setConn] = useState('');
@@ -21,34 +21,33 @@ const TutorAside = () => {
     let [screenType, setScreenType] = useState(screenLarge);
     let [isVisuals, setIsVisuals] = useState(true)
 
-    let [visuals, setVisuals ]= useState(true)
+    let [visuals, setVisuals] = useState(true)
     let [uname, setUname] = useState('')
 
     let handleChat = e => {
-        if(mssg !== null){
-            let mssg_cnt = 
+        if (mssg !== null) {
+            let mssg_cnt =
 
-            <div className="TutorMessageCnt">
-                <div className='TutorMessageCntContent'>
-                    {mssg}
+                <div className="TutorMessageCnt">
+                    <div className='TutorMessageCntContent'>
+                        {mssg}
+                    </div>
                 </div>
-            </div>
 
             setMssgList(item => [...item, mssg_cnt])
             document.querySelector('#TutorChatTextarea').value = '';
-
         }
-        
+
     }
 
-    
+
 
     let handleVideoResize = e => {
         let element = document.querySelector('.TutorAsideVideoCnt');
-        if(element.hasAttribute('id')){
+        if (element.hasAttribute('id')) {
             element.removeAttribute('id')
             setScreenType(screenLarge)
-        }else{
+        } else {
             element.setAttribute('id', 'TutorAsideVideoCnt')
             setScreenType(screenNormal)
 
@@ -60,44 +59,44 @@ const TutorAside = () => {
     let handleVidActions = e => {
 
         visuals.getVideoTracks()[0].enabled =
-        !(visuals.getVideoTracks()[0].enabled);
+            !(visuals.getVideoTracks()[0].enabled);
         /*navigator.mediaDevices.getUserMedia()
         .then((mediaStream) => {
             mediaStream.getVideoTracks()[0].enabled =
             !(mediaStream.getVideoTracks()[0].enabled);
         })*/
 
-        if(e.target.hasAttribute('id')){
+        if (e.target.hasAttribute('id')) {
             e.target.removeAttribute('id')
-        }else{
+        } else {
             e.target.setAttribute('id', 'collab-action')
         }
 
-        
+
     }
 
     let handleAudioActions = e => {
 
         visuals.getAudioTracks()[0].enabled =
-        !(visuals.getAudioTracks()[0].enabled);
+            !(visuals.getAudioTracks()[0].enabled);
         /*navigator.mediaDevices.getUserMedia()
         .then((mediaStream) => {
             mediaStream.getVideoTracks()[0].enabled =
             !(mediaStream.getVideoTracks()[0].enabled);
         })*/
 
-        if(e.target.hasAttribute('id')){
+        if (e.target.hasAttribute('id')) {
             e.target.removeAttribute('id')
-        }else{
+        } else {
             e.target.setAttribute('id', 'collab-action')
         }
-        
+
     }
 
-   
-    
+
+
     useEffect(() => {
- 
+
 
         let myVideo = document.querySelector('.tutor-video-tab');
         let room_id = '1234567890asdfghjkl';
@@ -114,45 +113,45 @@ const TutorAside = () => {
             video: true,
             audio: true
         })
-        .then(stream => { 
+            .then(stream => {
 
-            setVisuals(stream)
-            addVideoStream(myVideo, stream);
-            peer.on('call', call => {
-              
-
-                setVideoLoader('Connecting...')
-                call.answer(stream);
-                call.on('stream', userVideoStream => {
-                    setVideoLoader('')
-                    addVideoStream(myVideo, userVideoStream)
-                })
-            })
-
-            socket.on('user-connected', user_id => {
-                connectToNewUser(user_id, stream)
+                setVisuals(stream)
+                addVideoStream(myVideo, stream);
                 peer.on('call', call => {
-                    
+
+
                     setVideoLoader('Connecting...')
-                    call.answer(stream)
+                    call.answer(stream);
                     call.on('stream', userVideoStream => {
+                        setVideoLoader('')
                         addVideoStream(myVideo, userVideoStream)
                     })
                 })
+
+                socket.on('user-connected', user_id => {
+                    connectToNewUser(user_id, stream)
+                    peer.on('call', call => {
+
+                        setVideoLoader('Connecting...')
+                        call.answer(stream)
+                        call.on('stream', userVideoStream => {
+                            addVideoStream(myVideo, userVideoStream)
+                        })
+                    })
+                })
             })
-        })
-        .catch(e => console.log(e));
+            .catch(e => console.log(e));
 
         socket.on('user-disconnected', user_id => {
             if (peers[user_id]) peers[user_id].close()
         })
-          
+
         peer.on('open', id => {
             socket.emit('join-room', room_id, id)
         })
 
         function connectToNewUser(userId, stream) {
-            
+
             const call = peer.call(userId, stream);
             setVideoLoader('Connecting...')
             call.on('stream', userVideoStream => {
@@ -160,27 +159,27 @@ const TutorAside = () => {
             })
 
             call.on('close', () => {
-              myVideo.src = ''
+                myVideo.src = ''
             })
-          
+
             peers[userId] = call
         }
-          
+
         function addVideoStream(video, stream) {
             console.log(stream)
             video.srcObject = stream
             setVideoLoader('Connecting...')
             video.addEventListener('loadedmetadata', () => {
-              video.play()
-              setVideoLoader('')
+                video.play()
+                setVideoLoader('')
             })
         }
 
-        
+
     }, [location, peerId])
 
 
-    return ( 
+    return (
         <>
             <div className="TutorAside shadow-sm">
 
@@ -188,16 +187,16 @@ const TutorAside = () => {
                     <video className='tutor-video-tab'>
                     </video>
                     <ul>
-                        <li  className="video-size" style={{background: '#efefef', opacity: '.4', padding: '5px', borderRadius: '8px'}} onClick={handleVideoResize}>
-                            <img src={screenType} style={{height: '20px', width: '20px'}} alt="..." />
+                        <li className="video-size" style={{ background: '#efefef', opacity: '.4', padding: '5px', borderRadius: '8px' }} onClick={handleVideoResize}>
+                            <img src={screenType} style={{ height: '20px', width: '20px' }} alt="..." />
                         </li>
-                        <li className="disable-visuals" onClick={e=> handleVidActions(e)}>
-                            <img src={DiableVideoImage} style={{height: '25px', width: '25px'}} alt="..." />
+                        <li className="disable-visuals" onClick={e => handleVidActions(e)}>
+                            <img src={DiableVideoImage} style={{ height: '25px', width: '25px' }} alt="..." />
 
                         </li>
 
-                        <li className="disable-visuals" onClick={e=> handleAudioActions(e)}>
-                            <img src={muteSvg} style={{height: '25px', width: '25px'}} alt="..." />
+                        <li className="disable-visuals" onClick={e => handleAudioActions(e)}>
+                            <img src={muteSvg} style={{ height: '25px', width: '25px' }} alt="..." />
 
                         </li>
                     </ul>
@@ -212,14 +211,14 @@ const TutorAside = () => {
                         }
 
                     </div>
-                    <div className="TutorAsideChatControl" style={{background: '#fff'}}>
-                        <span style={{width: '80%',height: '80%', float: 'left', background: '#fff'}}>
-                            
-                            <textarea type="text" id='TutorChatTextarea'  style={{width: '100%', borderRadius: '5px', border: 'none', display: 'flex', alignItems: 'center', background: '#f9f9f9', height: '40px', padding: '10px 5px 5px 5px', fontFamily: 'serif', fontSize: 'medium', outline: 'none', resize: 'none'}} onInput={e => setMssg(e.target.value)} placeholder='Type Your Message Here'></textarea>
+                    <div className="TutorAsideChatControl" style={{ background: '#fff' }}>
+                        <span style={{ width: '80%', height: '80%', float: 'left', background: '#fff' }}>
+
+                            <textarea type="text" id='TutorChatTextarea' style={{ width: '100%', borderRadius: '5px', border: 'none', display: 'flex', alignItems: 'center', background: '#f9f9f9', height: '40px', padding: '10px 5px 5px 5px', fontFamily: 'serif', fontSize: 'medium', outline: 'none', resize: 'none' }} onInput={e => setMssg(e.target.value)} placeholder='Type Your Message Here'></textarea>
 
                         </span>
-                        <span style={{width: '20%', height: '70%', float: 'right', background: '#fff'}}>
-                            <button style={{height: '40px', width: '90%'}} onClick={handleChat}>
+                        <span style={{ width: '20%', height: '70%', float: 'right', background: '#fff' }}>
+                            <button style={{ height: '40px', width: '90%' }} onClick={handleChat}>
                                 send
                             </button>
                         </span>
@@ -228,7 +227,7 @@ const TutorAside = () => {
 
             </div>
         </>
-     );
+    );
 }
- 
+
 export default TutorAside;
